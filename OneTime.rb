cb719b8f672eb871b58a9edfa7d8a113d0520e2a
boccia <fboccacini@gmail.com>
2020-03-08 12:35:44 +0900

    refactoring, readme

--- OneTime.rb
+++ OneTime.rb
@@ -6,84 +6,108 @@ require 'net/https'
 
 class OneTime
 
-  attr_reader :password
+  attr_accessor :step, :encryption, :length
+  attr_reader :password, :secret
 
   def initialize(secret, length: 6, step: 30, encryption: 'SHA512')
 
     @secret = secret
+    @length = length
     @step = step
     @encryption = encryption
-    @length = length
-
-    self.generate
 
   end
 
-  def step
-    return @step
+  def generate(time: Time.now,
+                length: self.length,
+                step: self.step,
+                encryption: self.encryption)
+
+    begin
+
+      # Binary key from time / step
+      key = ['%0.16x' % (time.to_i / step).to_s(16).hex].pack('H*')
+
+      # Convert secret's non base32 char to base32 and back to string
+      data = Base32.decode(Base32.encode(self.secret))
+
+      # Get initial HMAC hash
+      hash = OpenSSL::HMAC.digest(encryption, data, key)
+
+      # Dynamic truncation:
+      # Get the last for bit of the last byte of the hash
+      offset = hash[hash.length - 1].ord & 0xf
+
+      # Get the for bytes pointed by the offset
+      hash = hash[offset .. offset + 3].bytes
+
+      # Get new hash
+      truncated_hash = ((hash[0] & 0x7f) << 24) |
+                       ((hash[1] & 0xff) << 16) |
+                       ((hash[2] & 0xff) <<  8) |
+                       (hash[3] & 0xff)
+
+      # Get actual password
+      @password = "%0.#{length}d" % (truncated_hash % (10 ** length));
+      return self.password
+
+    rescue Exception => e
+      puts e.message
+      # puts e.backtrace.join("\n")
+      return nil
+    end
+
   end
 
-  def secret
-    return @secret
-  end
+  def verify(password, time: Time.now,
+                      length: self.length,
+                      step: self.step,
+                      encryption: self.encryption)
 
-  def length
-    return @length
-  end
+    # verify a password correctness for a given time
+    begin
 
-  def encryption
-    return @encryption
-  end
+      return self.generate(time: time,
+                          length: length,
+                          step: step,
+                          encryption: encryption) == password
 
-  def generate(time: Time.now)
-
-    # Binary key from time / step
-    key = ['%0.16x' % (time.to_i / self.step).to_s(16).hex].pack('H*')
-
-    # Convert secret's non base32 char to base32 and back to string
-    data = Base32.decode(Base32.encode(self.secret))
-
-    # Get initial HMAC hash
-    hash = OpenSSL::HMAC.digest(self.encryption, data, key)
-
-    # Dynamic truncation:
-    # Get the last for bit of the last byte of the hash
-    offset = hash[hash.length - 1].ord & 0xf
-
-    # Get the for bytes pointed by the offset
-    hash = hash[offset .. offset + 3].bytes
-
-    # Truncate
-    truncated_hash = ((hash[0] & 0x7f) << 24) |
-                     ((hash[1] & 0xff) << 16) |
-                     ((hash[2] & 0xff) <<  8) |
-                     (hash[3] & 0xff)
-
-    # Get actual password
-    @password = "%0.#{self.length}d" % (truncated_hash % (10 ** self.length));
-    return self.password
+    rescue Exception => e
+      puts e.message
+      # puts e.backtrace.join("\n")
+      return false
+    end
 
   end
 
   def call_service(url, user, payload, content_type: 'application/json', accept: '*/*')
 
-    # Base64 encode authentication for the header
-    auth = Base64.strict_encode64("#{user}:#{rOTP}".encode("ASCII"))
+    begin
 
-    # Prep uri and headers
-    uri = URI.parse(url)
-    headers = {
-        'Authorization' => "Basic #{auth}",
-        'Content-Type' => content_type,
-        'Accept' => accept
-      }
+      self.generate if self.password.nil?
 
-    # Create request
-    request = Net::HTTP.new(uri.host, uri.port)
-    request.use_ssl = true
+      # Base64 encode authentication for the header
+      auth = Base64.strict_encode64("#{user}:#{self.password}".encode("ASCII"))
 
-    # Send request
-    return request.post(uri.path, payload.to_json, headers)
+      # Prep uri and headers
+      uri = URI.parse(url)
+      headers = {
+          'Authorization' => "Basic #{auth}",
+          'Content-Type' => content_type,
+          'Accept' => accept
+        }
+
+      # Create request
+      request = Net::HTTP.new(uri.host, uri.port)
+      request.use_ssl = true
+
+      # Send request
+      return request.post(uri.path, payload.to_json, headers)
+    rescue Exception => e
+      puts e.message
+      # puts e.backtrace.join("\n")
+      return nil
+    end
 
   end
 
